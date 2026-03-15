79d7f54aa6ba0fc204a0c08858bcd9a3f43c1836
Cedix09 <yura09e@gmail.com>
2026-03-15 23:34:28 +0500

    Добавил ссылки в footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-<!-- footer.php -->
 <footer>
-    <p>&copy; 2025 Мой проект. Все права защищены.</p>
+    <p>&copy; 2025 Мой проект</p>
+    <a href="/about">О нас</a> | <a href="/contact">Контакты</a>
 </footer>
+
